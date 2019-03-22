af4891ad7ccafc5b99a034be9dafb5928ed13cc5
Daniel Cochran <daniel.cochran61@gmail.com>
2019-03-22 13:05:29 -0400

    make base components folder

---
+++ client/src/components/homepage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class HomePage extends React.Component {
+    state = {
+
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default HomePage;
