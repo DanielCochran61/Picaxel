490b6f0f07702b658ee8a9647ba4d71fb85f7604
Haoyuan Lin <specialemon@gmail.com>
2019-03-27 11:44:32 -0400

    test1

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,15 +1,16 @@
 import React from "react";
 import UserContext from "../context/UserContext";
-
+import MainCanvas from "./MainCanvas";
 const HomePage = (props) => (
-	<UserContext.Consumer>
-		{context => {
-			return <div>
-				<h1>Lets Start Making Art Toegther!! (protected)</h1>
-				<h2>Welcome, {context.user.username}!</h2>
-			</div>
-		}}
-	</UserContext.Consumer>
+	<MainCanvas />
+	// <UserContext.Consumer>
+	// 	{context => {
+	// 		return <div>
+	// 			<h1>Lets Start Making Art Toegther!! (protected)</h1>
+	// 			<h2>Welcome, {context.user.username}!</h2>
+	// 		</div>
+	// 	}}
+	// </UserContext.Consumer>
 );
 
 export default HomePage;
