3058643de764186c0473ba3791d0c795fdb49a08
Haoyuan Lin <specialemon@gmail.com>
2019-03-29 12:53:51 -0400

    url test 15

--- client/src/components/MainCanvas.js
+++ client/src/components/MainCanvas.js
@@ -9,7 +9,7 @@ import io from 'socket.io-client';
 
 const remoteURL = "https://whispering-crag-56456.herokuapp.com";
 
-const socket = io.connect("http://whispering-crag-56456.herokuapp.com");
+const socket = io.connect("https://whispering-crag-56456.herokuapp.com");
 
 class MainCanvas extends Component {
 
